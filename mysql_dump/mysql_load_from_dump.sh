75f4cb332eb713e51b746fc21a5919ea151677e0
rohit <rohitangsudas91@gmail.com>
2018-11-25 18:17:54 +0800

    adding the mysql dumps and load from dump sh files

---
+++ mysql_dump/mysql_load_from_dump.sh
@@ -0,0 +1,5 @@
+mysql -h localhost -u root -pwolverin test_db < category
+mysql -h localhost -u root -pwolverin test_db < categorylinks
+mysql -h localhost -u root -pwolverin test_db < externallinks
+mysql -h localhost -u root -pwolverin test_db < page
+mysql -h localhost -u root -pwolverin test_db < pagelinks
